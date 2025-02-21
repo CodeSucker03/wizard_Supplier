04a3c365e20dfa33f00a36de8db7ad12aa229b97
huyquoc1210 <contact.huyquoc@gmail.com>
2025-02-21 13:37:08 +0700

    add colorPicker

---
+++ webapp/controller/<div style="width:600px;height:400px;bac.js
@@ -0,0 +1,14 @@
+<div style="width:600px;height:400px;background-image:url(data:image/png;base64,{{IMAGE_STRING}});background-size:600px 400px;background-repeat:no-repeat;position:relative;display:flex;">
+    <div style="position:absolute;font-size:{{MCC_FONT_SIZE}};transform:translate({{MCC_X}}px,{{MCC_Y}}px );color:{{
+      MCC_COLOR
+    };text-transform:uppercase;">{{MCC_TITLE}}</div>
+    <div style="position:absolute;font-size:{{NAME_SIZE}};transform:translate({{NAME_X}}px,{{NAME_Y}}px );color:{{
+      NAME_COLOR
+    };text-transform:uppercase;">{{NAME_TITLE}}</div>
+    <div style="position:absolute;font-size:{{COURSE_SIZE}};transform:translate({{COURSE_X}}px,{{COURSE_Y}}px );color:{{
+      COURSE_COLOR
+    };text-transform:uppercase;">{{COURSE_TITLE}}</div>
+    <div style="position:absolute;font-size:{{DATE_SIZE}};transform:translate({{DATE_X}}px,{{DATE_Y}}px );color:{{
+      DATE_COLOR
+  }};text-transform:uppercase;">{{DATE_TITLE}}</div>
+  </div>
